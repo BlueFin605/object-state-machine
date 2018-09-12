7868e52d2ad8ac32f01bc934a6e60b98c445ff47
BlueFin605 <bluefin605@gmail.com>
2018-09-12 21:29:44 +1200

    converted to use classes

--- src/collections/statemachinecollection.js
+++ src/collections/statemachinecollection.js
@@ -1,35 +1,32 @@
 var sm = require('../statemachine.js')
 
-function StateMachineCollection (name, states) {
-  this.collection = new Map()
-  this.name = name
-  this.states = states
-}
-
-StateMachineCollection.prototype.addState = function (key, state) {
-  if (this.collection.has(key)) {
-    console.log(`${key} already exists in collection`)
-    return
+class StateMachineCollection {
+  constructor (name, states) {
+    this.collection = new Map()
+    this.name = name
+    this.states = states
   }
-
-  var statemachine = new sm.StateMachine(this.name, this.states)
-  statemachine.setState(state)
-  this.collection.set(key, statemachine)
-}
-
-StateMachineCollection.prototype.persistState = function (callback) {
-  for (var [key, value] of this.collection) {
-    value.persistState((state, data) => callback(key, state, data))
+  addState (key, state) {
+    if (this.collection.has(key)) {
+      console.log(`${key} already exists in collection`)
+      return
+    }
+    var statemachine = new sm.StateMachine(this.name, this.states)
+    statemachine.setState(state)
+    this.collection.set(key, statemachine)
   }
-}
-
-StateMachineCollection.prototype.changeState = function (key, callback) {
-  if (this.collection.has(key) === false) {
-    console.log(`${key} does not exist in collection`)
-    return
+  persistState (callback) {
+    for (var [key, value] of this.collection) {
+      value.persistState((state, data) => callback(key, state, data))
+    }
   }
-
-  this.collection.get(key).changeState(callback)
+  changeState (key, callback) {
+    if (this.collection.has(key) === false) {
+      console.log(`${key} does not exist in collection`)
+      return
+    }
+    this.collection.get(key).changeState(callback)
+  }
 }
 
 module.exports.StateMachineCollection = StateMachineCollection
--- src/statemachine.js
+++ src/statemachine.js
@@ -1,49 +1,45 @@
 // My module
-function StateMachine (name, statesFactory) {
-  this.name = name
-  this.statesFactory = statesFactory
-  this.currentState = null
-  this.currentData = {}
-  this.persistStateCallback = null
-}
-
-StateMachine.prototype.setState = function (state) {
-  console.log(`sm set state to ${state.name}`)
-  this.currentState = state.state
-  this.currentData = state.data
-}
-
-StateMachine.prototype.persistState = function (callback) {
-  this.persistStateCallback = callback
-}
-
-StateMachine.prototype.changeState = function (callback) {
-  // take a copy of the data, so if a state wants to chnage it, it needs to chnage state(to itself if necessary)
-  var dataCopy = JSON.parse(JSON.stringify(this.currentData))
-
-  var newState = callback(this.currentState, dataCopy)
-  if (newState === null || newState.state === null) {
-    return
+class StateMachine {
+  constructor (name, statesFactory) {
+    this.name = name
+    this.statesFactory = statesFactory
+    this.currentState = null
+    this.currentData = {}
+    this.persistStateCallback = null
   }
-
-  if (newState.state === this.currentState) { return }
-
-  console.log(`sm changing state from ${this.currentState.name} to ${newState.state.name}`)
-  this.currentState = newState.state
-  this.currentData = newState.data
-
-  if (this.persistStateCallback !== null) {
-    // we want to send a copy since we want to prevent caller changing the state
-    dataCopy = JSON.parse(JSON.stringify(this.currentData))
-    this.persistStateCallback(this.currentState, dataCopy)
+  setState (state) {
+    console.log(`sm set state to ${state.name}`)
+    this.currentState = state.state
+    this.currentData = state.data
   }
-}
-
-StateMachine.prototype.createNextState = function (name, data) {
-  // console.log(`create next state of ${name}:${data}`);
-  return {
-    state: this.statesFactory[name].create(this),
-    data: data
+  persistState (callback) {
+    this.persistStateCallback = callback
+  }
+  changeState (callback) {
+    // take a copy of the data, so if a state wants to chnage it, it needs to chnage state(to itself if necessary)
+    var dataCopy = JSON.parse(JSON.stringify(this.currentData))
+    var newState = callback(this.currentState, dataCopy)
+    if (newState === null || newState.state === null) {
+      return
+    }
+    if (newState.state === this.currentState) {
+      return
+    }
+    console.log(`sm changing state from ${this.currentState.name} to ${newState.state.name}`)
+    this.currentState = newState.state
+    this.currentData = newState.data
+    if (this.persistStateCallback !== null) {
+      // we want to send a copy since we want to prevent caller changing the state
+      dataCopy = JSON.parse(JSON.stringify(this.currentData))
+      this.persistStateCallback(this.currentState, dataCopy)
+    }
+  }
+  createNextState (name, data) {
+    // console.log(`create next state of ${name}:${data}`);
+    return {
+      state: this.statesFactory[name].create(this),
+      data: data
+    }
   }
 }
 
